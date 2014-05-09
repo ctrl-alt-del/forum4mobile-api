dd202f71776c76ea1efd2b0f3bd30e6022b603eb
ctrl-alt-del <jiyang881230@gmail.com>
2014-05-09 00:10:46 -0700

    add routes and group them with version control under /api/v1;

--- app/routes.php
+++ app/routes.php
@@ -11,7 +11,34 @@
 |
 */
 
-Route::get('/', function()
-{
-	return View::make('hello');
-});
+/**
+* Put all the routes into a group 'api/v1'
+*
+* @since 2014-05-08
+* @version 1.0
+**/
+Route::group(
+	array(
+		'prefix'    => 'api/v1',
+        'before'    => '', // filter used before calling routes
+        'after'     => '', // filter used after calling routes
+        ),
+
+	function() {
+
+		Route::get('/', function()
+		{
+			return 'Welcome to <b>forum4mobile-api</b> demo version 01';
+		});
+
+		Route::resource('users', 'UsersController');
+        // Route::post('users/register', array('uses' => 'UsersController@store'));
+        // Route::post('users/login', array('uses' => 'UsersController@doLogin')); // create a new user
+        // Route::get('users/logout', array('uses' => 'UsersController@doLogout'));
+        // Route::get('users/{id}', array('before' => 'auth.users', 'uses' => 'UsersController@show'));
+
+		Route::resource('topics', 'TopicsController');
+		Route::resource('responses', 'ResponsesController');
+		Route::resource('votes', 'VotesController');
+	});
+

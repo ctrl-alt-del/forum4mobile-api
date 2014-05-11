525c1ad999af29a91aa164a6d745b76d079a4a6b
ctrl-alt-del <jiyang881230@gmail.com>
2014-05-11 01:29:40 -0700

    update seeder of votes to truncate the table and auto generate dummy data to it;

--- app/database/seeds/VotesTableSeeder.php
+++ app/database/seeds/VotesTableSeeder.php
@@ -4,8 +4,27 @@ use Illuminate\Database\Schema\Blueprint;
 
 class VotesTableSeeder extends Seeder {
 
-    public function run()
-    {
-        
+    public function run() {
+        DB::statement('SET FOREIGN_KEY_CHECKS = 0');
+
+        // Uncomment the below to wipe the table clean before populating
+        DB::table('votes')->truncate();
+
+        DB::statement('SET FOREIGN_KEY_CHECKS = 1');
+        $date = new DateTime;
+
+        for ($i = 0; $i < 10; $i++) {
+            $vote = array(
+                'user_id' => rand(1, 4), 
+                'review_id' => rand(1, 10),
+                'created_at' => $date,
+                'updated_at' =>$date,
+                );
+            try {
+                DB::table('votes')->insert($vote);
+            } catch (Exception $e) {
+
+            }
+        }
     }
 }

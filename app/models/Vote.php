908670dc6ecfed02f7ce6c6620845cda7a994a07
ctrl-alt-del <jiyang881230@gmail.com>
2014-05-11 01:24:52 -0700

    update model of concerns to declare its relation with users and reviews;

--- app/models/Vote.php
+++ app/models/Vote.php
@@ -2,5 +2,21 @@
 
 class Vote extends Eloquent {
     
+    protected $appends = array('location');//'user', 'responses_count','concerns_count');
+    protected $hidden = array();
+
+   
+
+    public function getLocationAttribute() {
+        return URL::to('api/v1/votes/' . $this->id);
+    }
+
+    public function user() {
+        return $this->belongsTo('User');
+    }
+
+     public function review() {
+        return $this->belongsTo('Review');
+    }
 
 }

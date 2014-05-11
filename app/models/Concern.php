6e0d8454ce376d77d4e69d570552b2c6a635400d
ctrl-alt-del <jiyang881230@gmail.com>
2014-05-11 01:22:32 -0700

    update model of concerns to declare its relation with users and topics, and also to append the location property;

--- app/models/Concern.php
+++ app/models/Concern.php
@@ -2,5 +2,21 @@
 
 class Concern extends Eloquent {
     
+    protected $appends = array('location');//'user', 'responses_count','concerns_count');
+    protected $hidden = array();
+
+   
+
+    public function getLocationAttribute() {
+        return URL::to('api/v1/concerns/' . $this->id);
+    }
+
+    public function user() {
+        return $this->belongsTo('User');
+    }
+
+     public function topic() {
+        return $this->belongsTo('Topic');
+    }
 
 }

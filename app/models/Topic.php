e28a4b9c2d1e258a5fa8c6959669c4bad051d959
ctrl-alt-del <jiyang881230@gmail.com>
2014-05-11 01:21:22 -0700

    update model of topics to include concerns count;

--- app/models/Topic.php
+++ app/models/Topic.php
@@ -1,14 +1,57 @@
 <?php
 
 class Topic extends Eloquent {
+
+    // protected $guarded = array('id');
+    // public static $rules = array(
+    //     'user_id' => 'required|exists:users,id',
+    //     'title' => 'min:3',
+    //     'description' => 'min:3',
+    //     'content' => 'min:3',
+    // );
     
-    protected $appends = array('location');
-    protected $hidden = array();
+    protected $appends = array('location', 'concerns_count');//'user', 'responses_count','concerns_count');
+    protected $hidden = array('concerns');
+
+    // /**
+    //  * @return mixed
+    //  */
+    // public function validate() {
+    //     return Validator::make($this->toArray(), self::$rules);
+    // }
+
+    // public function responses() {
+    //     return $this->hasMany('Response');
+    // }
+
+    public function concerns() {
+        return $this->hasMany('Concern');
+    }
+
+    // public function includeResponses() {
+    //     $this->hidden = array_diff($this->hidden, array('responses'));
+    // }
+
+    // public function getResponsesCount() {
+    //     return count($this->responses);
+    // }
+
+    public function getConcernsCountAttribute() {
+        return count($this->concerns);
+    }
 
     public function getLocationAttribute() {
+//        return URL::to('products?' . 'upc=' . $this->upc);
         return URL::to('api/v1/topics/' . $this->id);
     }
+
+	// public function getImageAttribute($image)
+	// {
+	// 	return URL::to('images/'.$image);
+	// }
 
     public function user() {
         return $this->belongsTo('User');
     }
+
+}

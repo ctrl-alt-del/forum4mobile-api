14647f02f7fdebc0bbde0bb38003d8691d16bd76
ctrl-alt-del <jiyang881230@gmail.com>
2014-05-11 01:28:41 -0700

    update seeder of concerns to truncate the table and auto generate dummy data to it;

--- app/database/seeds/ConcernsTableSeeder.php
+++ app/database/seeds/ConcernsTableSeeder.php
@@ -5,8 +5,27 @@ use Illuminate\Database\Schema\Blueprint;
 
 class ConcernsTableSeeder extends Seeder {
 
-    public function run()
-    {
-        
+    public function run() {
+        DB::statement('SET FOREIGN_KEY_CHECKS = 0');
+
+        // Uncomment the below to wipe the table clean before populating
+        DB::table('concerns')->truncate();
+
+        DB::statement('SET FOREIGN_KEY_CHECKS = 1');
+        $date = new DateTime;
+
+        for ($i = 0; $i < 20; $i++) {
+            $concern = array(
+                'user_id' => rand(1, 4), 
+                'topic_id' => rand(1, 10),
+                'created_at' => $date,
+                'updated_at' =>$date,
+                );
+            try {
+                DB::table('concerns')->insert($concern);
+            } catch (Exception $e) {
+
+            }
+        }
     }
 }

d5524e3fcdce67a8a26d3ab1b8a7711e3c16eac2
ctrl-alt-del <jiyang881230@gmail.com>
2014-05-11 01:29:07 -0700

    update seeder of reviews to truncate the table and auto generate dummy data to it;

--- app/database/seeds/ReviewsTableSeeder.php
+++ app/database/seeds/ReviewsTableSeeder.php
@@ -4,8 +4,28 @@ use Illuminate\Database\Schema\Blueprint;
 
 class ReviewsTableSeeder extends Seeder {
 
-    public function run()
-    {
-      
+    public function run() {
+        DB::statement('SET FOREIGN_KEY_CHECKS = 0');
+
+        // Uncomment the below to wipe the table clean before populating
+        DB::table('reviews')->truncate();
+
+        DB::statement('SET FOREIGN_KEY_CHECKS = 1');
+        $date = new DateTime;
+
+        for ($i = 0; $i < 15; $i++) {
+            $review = array(
+                'user_id' => rand(1, 4), 
+                'topic_id' => rand(1, 10),
+                'content' => "some responses $i",
+                'created_at' => $date,
+                'updated_at' =>$date,
+                );
+            try {
+                DB::table('reviews')->insert($review);
+            } catch (Exception $e) {
+
+            }
+        }
     }
 }

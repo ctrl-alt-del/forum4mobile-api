6a95c9389281fd178246d1b45c6e2e8219126d1c
ctrl-alt-del <jiyang881230@gmail.com>
2014-05-12 00:31:09 -0700

    add store() method to TopicsController;

--- app/controllers/api/v1/TopicsController.php
+++ app/controllers/api/v1/TopicsController.php
@@ -17,7 +17,48 @@ class TopicsController extends \BaseController {
 	 * @return Response
 	 */
 	public function store() {
+		$rules = array(
+			'title'	   => 'required',
+			'description'    => 'required', 
+			'content' => 'required',
+			'user_id' => 'required'
+			);
 
+		// run the validation rules on the inputs from the form
+		$validator = Validator::make(Input::all(), $rules);
+
+		// if the validator fails, redirect back to the form
+		if ($validator->fails()) {
+			return Response::json(array(
+				'success'   => false,
+				'error'     => true,
+				'type'      => '403',
+				'message'   => $validator->message()->all(),
+				));
+		}
+		
+		// store
+		$topic = new Topic;
+		$topic->title 		= Input::get('title');
+		$topic->description = Input::get('description');
+		$topic->content   	= Input::get('content');
+		$topic->user_id   	= Input::get('user_id');
+
+		if ($topic->save()) {
+			return Response::json(array(
+				'success'   => true,
+				'error'     => false,
+				'type'      => '201',
+				'message'   => 'new topic has created',
+				));
+		} else {
+			return Response::json(array(
+				'success'   => false,
+				'error'     => true,
+				'type'      => '403',
+				'message'   => 'cannot create new topic',
+				));
+		}
 	}
 
 

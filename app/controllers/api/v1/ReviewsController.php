46d831ee6395ad007e64caf37a3dc468fd9a6eb7
ctrl-alt-del <jiyang881230@gmail.com>
2014-05-12 00:47:58 -0700

    add store() method to ReviewsController;

--- app/controllers/api/v1/ReviewsController.php
+++ app/controllers/api/v1/ReviewsController.php
@@ -18,6 +18,46 @@ class ReviewsController extends \BaseController {
 	 * @return Response
 	 */
 	public function store() {
+		$rules = array(
+			'topic_id'    => 'required', 
+			'content' => 'required',
+			'user_id' => 'required'
+			);
+
+		// run the validation rules on the inputs from the form
+		$validator = Validator::make(Input::all(), $rules);
+
+		// if the validator fails, redirect back to the form
+		if ($validator->fails()) {
+			return Response::json(array(
+				'success'   => false,
+				'error'     => true,
+				'type'      => '403',
+				'message'   => $validator->message()->all(),
+				));
+		}
+		
+		// store
+		$review = new Review;
+		$review->topic_id = Input::get('topic_id');
+		$review->content   	= Input::get('content');
+		$review->user_id   	= Input::get('user_id');
+
+		if ($review->save()) {
+			return Response::json(array(
+				'success'   => true,
+				'error'     => false,
+				'type'      => '201',
+				'message'   => 'new review has created',
+				));
+		} else {
+			return Response::json(array(
+				'success'   => false,
+				'error'     => true,
+				'type'      => '403',
+				'message'   => 'cannot create new review',
+				));
+		}
 	}
 
 

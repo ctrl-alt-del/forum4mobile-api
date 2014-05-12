26ed54c97d307be512cad2224105609bffab6600
ctrl-alt-del <jiyang881230@gmail.com>
2014-05-12 00:39:56 -0700

    add store() method to ConcernsController;

--- app/controllers/api/v1/ConcernsController.php
+++ app/controllers/api/v1/ConcernsController.php
@@ -17,6 +17,54 @@ class ConcernsController extends \BaseController {
 	 * @return Response
 	 */
 	public function store() {
+		$rules = array(
+			'topic_id'	=> 'required',
+			'user_id' 	=> 'required'
+			);
+
+		// run the validation rules on the inputs from the form
+		$validator = Validator::make(Input::all(), $rules);
+
+		// if the validator fails, redirect back to the form
+		if ($validator->fails()) {
+			return Response::json(array(
+				'success'   => false,
+				'error'     => true,
+				'type'      => '403',
+				'message'   => $validator->message()->all(),
+				));
+		}
+
+		$concern = Concern::where('user_id','=',$user_id)->where('topic_id','=',$topic_id)->get();
+		if ($concern != null) {
+			return Response::json(array(
+				'success'   => false,
+				'error'     => true,
+				'type'      => '403',
+				'message'   => 'you had expressed your concern',
+				));
+		}
+
+		// store
+		$concern = new Concern;
+		$concern->topic_id   	= Input::get('topic_id');
+		$concern->user_id   	= Input::get('user_id');
+
+		if ($topic->save()) {
+			return Response::json(array(
+				'success'   => true,
+				'error'     => false,
+				'type'      => '201',
+				'message'   => 'new topic has created',
+				));
+		} else {
+			return Response::json(array(
+				'success'   => false,
+				'error'     => true,
+				'type'      => '403',
+				'message'   => 'cannot create new topic',
+				));
+		}
 	}
 
 

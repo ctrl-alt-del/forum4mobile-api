c7312a78293be016995827fe3f710e404f175783
ctrl-alt-del <jiyang881230@gmail.com>
2014-05-13 00:09:52 -0700

    add show() method to ConcernsController;

--- app/controllers/api/v1/ConcernsController.php
+++ app/controllers/api/v1/ConcernsController.php
@@ -75,6 +75,18 @@ class ConcernsController extends \BaseController {
 	 * @return Response
 	 */
 	public function show($id) {
+		$topic = Concern::find($id);
+		if ($topic != null) {
+			return Response::json($topic);	
+		} else {
+			return Response::json(array(
+				'success'   => false,
+				'error'     => true,
+				'type'      => '403',
+				'message'   => 'cannot find this topic',
+				));
+		}
+		
 	}
 
 	/**
